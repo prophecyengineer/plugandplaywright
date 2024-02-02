a84781f800150c73962264fe2b8967735f34c937
lott-e <hi.lottie@icloud.com>
2024-02-02 12:39:43 +0000

    updating readme

---
+++ README.md
@@ -0,0 +1,10 @@
+> [!WARNING]
+>
+> ## Please bear with me this is my first package, currently testing it right now, it is not working!
+
+## ForePlaywright
+
+this will soon be a package that helps you cut down your test writing time.
+Loads of functions to simplify and beautify your workflow.
+
+Excited to know your thoughts.
---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getExactText = exports.getText = void 0;
+const locators_1 = require("./elements/text/locators");
+Object.defineProperty(exports, "getText", { enumerable: true, get: function () { return locators_1.getText; } });
+Object.defineProperty(exports, "getExactText", { enumerable: true, get: function () { return locators_1.getExactText; } });
